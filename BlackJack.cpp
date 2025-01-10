a33a5bf6a402625dd0081a55c96b31458fa02a38
Giolandoll <giolando6628@gmail.com>
2025-01-10 12:50:36 +0100

    Method 2 of doing the card and deck

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -1,17 +1,17 @@
-using namespace std;
 #include <iostream>
+#include <array>
+using namespace std;
 
-
-enum class Suits{
+// Enumeration for Suits
+enum class Suits {
     CLUBS,
     DIAMONDS,
     HEARTS,
     SPADES,
 };
 
-
-enum class CardNames{
-
+// Enumeration for Card Names
+enum class CardNames {
     ACE = 1,
     TWO,
     THREE,
@@ -21,104 +21,96 @@ enum class CardNames{
     SEVEN,
     EIGHT,
     NINE,
-    TEN, 
+    TEN,
     JACK,
     QUEEN,
     KING,
 };
 
-//Card class 
-class Card{
+// Card class
+class Card {
+private:
+    CardNames name;
+    Suits suit;
+    int value;
 
-    protected: 
-        CardNames name;
-        Suits suit;
-        int value;
+public:
+    // Constructor
+    Card(CardNames n = CardNames::ACE, Suits s = Suits::CLUBS, int v = 1)
+        : name(n), suit(s), value(v) {}
 
-    public: 
+    // Print the card details
+    void PrintCard() const {
+        PrintValue();
+        cout << " of ";
+        PrintSuit();
+        cout << endl;
+    }
 
-    Card(CardNames n = CardNames::ACE, Suits s = Suits::CLUBS, int v = 1) : name(n), suit(s), value(v) {}
-
-        void PrintCard(){
-
-            PrintValue();
-            cout << " of ";
-            PrintSuit();
-            cout << endl;
-
+    // Print the card's value
+    void PrintValue() const {
+        if (name == CardNames::JACK) {
+            cout << "jack";
+        } else if (name == CardNames::QUEEN) {
+            cout << "queen";
+        } else if (name == CardNames::KING) {
+            cout << "king";
+        } else if (name == CardNames::ACE) {
+            cout << "ace";
+        } else {
+            cout << value;
         }
+    }
 
-        void PrintValue(){
-
-            if (name == CardNames::JACK){
-                cout << "jack";
-            }else if (name == CardNames::QUEEN){
-                cout << "queen";
-            }else if (name == CardNames::KING){
-                cout << "king";
-            }else if(name == CardNames::ACE){
-                cout << "ace";
-            }else{
-                cout << value;
-            }
+    // Print the card's suit
+    void PrintSuit() const {
+        if (suit == Suits::CLUBS) {
+            cout << "clubs";
+        } else if (suit == Suits::DIAMONDS) {
+            cout << "diamonds";
+        } else if (suit == Suits::HEARTS) {
+            cout << "hearts";
+        } else if (suit == Suits::SPADES) {
+            cout << "spades";
         }
-
-        void PrintSuit(){
-            if (suit == Suits::CLUBS){
-                cout << "clubs";
-            }else if(suit == Suits::DIAMONDS){
-                cout << "diamonds";
-            }else if(suit == Suits::HEARTS){
-                cout << "hearts";
-            }else if(suit == Suits::SPADES){
-                cout << "spades";
-            }
-        }
+    }
 };
 
-//Deck class which inherits from parent Card
-class Deck : public Card{
+// Deck class (manages a collection of cards)
+class Deck {
+private:
+    array<Card, 52> arrCards; // Fixed-size array to store 52 cards
 
-    private: 
-        Card arrCards[52];
-
-
-    public:
-        void SetupCards(){
-            int index = 0;
-            for (int cols = (int)Suits::CLUBS; cols <= (int)Suits::SPADES; cols++){
-                for ( int row = (int)CardNames::ACE; row <= (int)CardNames::KING; row++){
-                    CardNames c = static_cast<CardNames>(row);
-                    Suits s = static_cast<Suits>(cols);
-                    
-                int cardValue = (row > 10) ? 10 : row; // JACK, QUEEN, KING -> 10
-                    arrCards[index++] = Card(c, s, cardValue);
-                    cout << (int)c << "of" << (int)s <<endl;
-                }
+public:
+    // Initialize the deck
+    void SetupCards() {
+        int index = 0;
+        for (int suit = (int)Suits::CLUBS; suit <= (int)Suits::SPADES; ++suit) {
+            for (int rank = (int)CardNames::ACE; rank <= (int)CardNames::KING; ++rank) {
+                CardNames cardName = static_cast<CardNames>(rank);
+                Suits cardSuit = static_cast<Suits>(suit);
+                int cardValue = (rank > 10) ? 10 : rank; // JACK, QUEEN, KING -> 10
+                arrCards[index++] = Card(cardName, cardSuit, cardValue);
             }
         }
+    }
 
-
-        void PrintAll(){
-            for (int cols = (int)Suits::CLUBS; cols <= (int)Suits::SPADES; cols++){
-                for ( int row = (int)CardNames::ACE; row <= (int)CardNames::KING; row++){         
-                    int index = (13*cols) + row -1;
-                    arrCards[index].PrintCard();
-                }
-                cout << endl;
-            }
+    // Print all cards in the deck
+    void PrintAll() const {
+        for (const auto& card : arrCards) {
+            card.PrintCard();
         }
-
+    }
 };
 
-
-
-int main(){
-
+// Main function
+int main() {
     Deck deck;
     deck.SetupCards();
 
     cout << endl;
 
     deck.PrintAll();
+
+    return 0;
 }
